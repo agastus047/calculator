ee0917b068a726b4409d016e487eeb29bcfb3cb9
Agastus Joyson <agastusjoyson@gmail.com>
2022-07-24 00:29:00 +0530

    Evaluate binary operations

--- script.js
+++ script.js
@@ -12,11 +12,47 @@ function divide(a,b) {
 }
 function operate(operator,a,b) {
     if(operator === '+')
-        return sum(a,b);
+        return add(a,b);
     else if(operator === '-')
         return subtract(a,b);
-    else if(operator === '*')
+    else if(operator === 'x')
         return multiply(a,b);
     else if(operator === '/')
         return divide(a,b);
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('click',display);
+});
+
+let value = '';
+let value1 = 0;
+let value2 = '';
+let operator = '';
+let result;
+function display(e) {
+    let display = document.querySelector(".display");
+    if(this.id === "CLEAR") {
+        display.textContent = '';
+        value = '';
+    }
+    else if(this.id === '+' || this.id === '-' || this.id === 'x' || this.id === '/') {
+        value1 = +value;
+        display.textContent += ` ${this.id} `;
+        operator = this.id;
+        value = '';
+    }
+    else if(this.id === 'equal') {
+        result = operate(operator,value1,value2);
+        display.textContent = result;
+    }
+    else {
+        display.textContent += this.id;
+        value += this.id;
+        console.log(value1); //console log
+    }
+    
+    value2 = +value;
+    console.log(value2); //console log
 }
